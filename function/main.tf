37e9ab3a3017eeb09c395ffdb2aba0d283259c55
Rafael Felix de Moraes <dickinson.rafael@gmail.com>
2024-02-14 21:33:41 -0300

    Creating Terraform resources for Azure App Functions

--- function/main.tf
+++ function/main.tf
@@ -1,53 +1,90 @@
-resource "azurerm_function_app" "example" {
-  name                      = "example-function-app"
-  resource_group_name       = azurerm_resource_group.example.name
-  location                  = azurerm_resource_group.example.location
-  app_service_plan_id       = azurerm_app_service_plan.example.id
-  storage_account_name      = azurerm_storage_account.example.name
-  storage_account_access_key = azurerm_storage_account.example.primary_access_key
-  version                   = "~3"
+# Create a resource group
+resource "azurerm_resource_group" "tc-g8-resource-group" {
+  name     = "tc-g8-resources"
+  location = var.region
+}
+
+# Create a storage account
+resource "azurerm_storage_account" "tc-g8-storage-account" {
+  name                     = "g8fiaptc"
+  resource_group_name      = azurerm_resource_group.tc-g8-resource-group.name
+  location                 = azurerm_resource_group.tc-g8-resource-group.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  account_kind             = "Storage"
+}
+
+# Create a service plan
+resource "azurerm_service_plan" "tc-g8-service-plan" {
+  name                = "tc-g8-service-plan"
+  location            = azurerm_resource_group.tc-g8-resource-group.location
+  resource_group_name = azurerm_resource_group.tc-g8-resource-group.name
+  os_type             = "Linux"
+  sku_name            = "Y1"
+}
+
+# Create a linux function app
+resource "azurerm_linux_function_app" "tc-g8-function-app" {
+  name                      = "tc-g8-function-app"
+  resource_group_name       = azurerm_resource_group.tc-g8-resource-group.name
+  location                  = azurerm_resource_group.tc-g8-resource-group.location
+  service_plan_id           = azurerm_service_plan.tc-g8-service-plan.id
+  
+  storage_account_name      = azurerm_storage_account.tc-g8-storage-account.name
+  storage_account_access_key = azurerm_storage_account.tc-g8-storage-account.primary_access_key
+
+  tags = var.tags
 
   app_settings = {
     "FUNCTIONS_WORKER_RUNTIME" = "node"
   }
-}
 
-resource "azurerm_app_service_plan" "example" {
-  name                = "example-app-service-plan"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-  kind                = "FunctionApp"
-
-  sku {
-    tier = "Dynamic"
-    size = "Y1"
+  site_config {
+    application_stack {
+      node_version = "18"
+    }
+    
   }
 }
 
-resource "azurerm_storage_account" "example" {
-  name                     = "examplestorageaccount"
-  resource_group_name      = azurerm_resource_group.example.name
-  location                 = azurerm_resource_group.example.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
+# Create a httpTrigger function app with custom source code
+resource "azurerm_function_app_function" "tc-g8-app-function" {
+  name            = "tc-g8-app-function" 
+  function_app_id = azurerm_linux_function_app.tc-g8-function-app.id
+  language        = "Javascript"
+
+  file {
+    name = "index.js"
+    content = file("./azure_function/src/functions/index.js")
+  }
+
+  file {
+    name = "host.json"
+    content = file("./azure_function/host.json")
+  }
+
+  config_json = jsonencode({
+    "bindings" = [
+      {
+        "authLevel" = "function"
+        "direction" = "in"
+        "name" = "req"
+        "type" = "httpTrigger"
+        "methods" = [
+          "get",
+          "post",
+        ]
+      },
+      {
+        "direction" = "out"
+        "name"      = "res"
+        "type"      = "http"
+      },
+    ]
+  })
 }
 
-resource "azurerm_function_app_blob_storage" "example" {
-  name                      = "example-blob-storage"
-  storage_account_name      = azurerm_storage_account.example.name
-  storage_account_access_key = azurerm_storage_account.example.primary_access_key
-}
-
-resource "azurerm_storage_account_function_app" "example" {
-  storage_account_name = azurerm_storage_account.example.name
-  function_app_name    = azurerm_function_app.example.name
-}
-
-resource "azurerm_function_app_zip" "example" {
-  function_app_id = azurerm_function_app.example.id
-  content         = "path/to/your/function_code.zip"  # Update with your actual zip file path
-}
-
+# Set a output after execution
 output "function_name" {
-  value = azurerm_function_app.example.id
+  value = azurerm_linux_function_app.tc-g8-function-app.id
 }
--- main.tf
+++ main.tf
@@ -1,29 +1,26 @@
+# Initialize Azure provider
 terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.0"
+      version = "~> 3"
     }
   }
 
 }
 
+# Config provider
 provider "azurerm" {
   skip_provider_registration = true # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
   features {}
 }
 
-# Create a resource group
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
-  location = var.region
-}
-
+# Create a module
 module "function" {
   source = "./function"
 }
 
-
+# Set a output after execution
 output "function_name" {
   value = module.function.function_name
 }

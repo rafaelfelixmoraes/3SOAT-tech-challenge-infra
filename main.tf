4e5532952d17fcbbcac8fe5966ea20d2ce0c0b43
Rafael Felix de Moraes <dickinson.rafael@gmail.com>
2024-02-18 19:31:05 -0300

    Fix identention

--- main.tf
+++ main.tf
@@ -23,8 +23,8 @@ resource "azurerm_resource_group" "tc-g8-resource-group" {
 
 # Create a module
 module "function" {
-  source = "./function"
-  depends_on = [ azurerm_resource_group.tc-g8-resource-group ]
+  source     = "./function"
+  depends_on = [azurerm_resource_group.tc-g8-resource-group]
 }
 
 # Set a output after execution

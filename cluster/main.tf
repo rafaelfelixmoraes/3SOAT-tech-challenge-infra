e6ab318c54be0bd885e6a906f9fd32c8f245093e
Wendell Alves <wendellmccaulay@hotmail.com>
2024-02-24 21:13:38 -0300

    Criando cluster pelo terraform local

--- cluster/main.tf
+++ cluster/main.tf
@@ -1,33 +1,45 @@
-resource "azurerm_resource_group" "tech-challenge" {
-  name     = "tech-challenge"
-  location = "West Europe"
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.93.0"
+    }
+  }
 }
 
-resource "azurerm_kubernetes_cluster" "example" {
-  name                = "example-aks1"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-  dns_prefix          = "exampleaks1"
+provider "azurerm" {
+  # Configuration options
+  features {
+    
+  }
+}
+
+resource "azurerm_resource_group" "tech-challenge" {
+  name     = var.resource_group_name
+  location = var.region
+}
+
+resource "azurerm_kubernetes_cluster" "tech-challenge-k8s" {
+  name                = var.cluster_name
+  location            = azurerm_resource_group.tech-challenge.location
+  resource_group_name = azurerm_resource_group.tech-challenge.name
+  dns_prefix          = var.cluster_name
 
   default_node_pool {
-    name       = "my-pool"
+    name       = var.pool_name
     node_count = 1
-    vm_size    = "Standard_B2S"
+    vm_size    = var.vm_size
   }
 
-  service_principal {
-    client_id     = "00000000-0000-0000-0000-000000000000"
-    client_secret = "00000000000000000000000000000000"
-  }
+  //service_principal {
+  //  client_id     = "0000000"
+  //  client_secret = "00000000"
+  //}
 }
 
-resource "azurerm_kubernetes_cluster_node_pool" "example" {
+resource "azurerm_kubernetes_cluster_node_pool" "cluster-node-pool" {
   name                  = "internal"
-  kubernetes_cluster_id = azurerm_kubernetes_cluster.example.id
-  vm_size               = "Standard_DS2_v2"
+  kubernetes_cluster_id = azurerm_kubernetes_cluster.tech-challenge-k8s.id
+  vm_size               = var.vm_size
   node_count            = 1
-
-  tags = {
-    Environment = "Production"
-  }
 }

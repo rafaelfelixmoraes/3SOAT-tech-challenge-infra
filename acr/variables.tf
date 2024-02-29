54fecfdd12481d7c50e34568fa88b55e4e7c3232
Wendell Alves <wendellmccaulay@hotmail.com>
2024-02-28 21:43:37 -0300

    Adicionar pasta para ACR

---
+++ acr/variables.tf
@@ -0,0 +1,11 @@
+variable "region" {
+    type = string
+    default = "East US"
+    description = "Main Region"
+}
+
+variable "resource_group_name" {
+    type = string
+    default = "acr-resource-group"
+    description = "Resource Group Name"
+}

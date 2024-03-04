d6c4de767e254e9c6d0ad47108afce0c760acfe3
Rafael Felix de Moraes <dickinson.rafael@gmail.com>
2024-03-04 20:36:00 -0300

    Adding request to EntraID login API

--- azure_function/src/functions/index.js
+++ azure_function/src/functions/index.js
@@ -1,12 +1,27 @@
 module.exports = async function (context, req) {
-    context.log(`Consultando dados do endereço para o CEP: ${req.query.cep}`);
-    
-    const requestTest = await fetch(`https://viacep.com.br/ws/${req.query.cep}/json`);
+    context.log(`Obtendo token no Azure EntraID`);
 
-    const responseTest = await requestTest.json();
+    const tenantID = "xxxxx";
+
+    const formData = new URLSearchParams();
+    formData.append("grant_type", "Client_Credentials");
+    formData.append("client_id", "xxxxx");
+    formData.append("client_secret", "xxxxx");
+    formData.append("scope", "https://aks-aad-server.azure.com/.default");
+    
+    const azureEntraRequest = await fetch(`https://login.microsoftonline.com/${tenantID}/oauth2/v2.0/token`, {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: formData.toString(),
+    json: true
+    });
+
+    const azureToken = await azureEntraRequest.json();
 
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: responseTest
+        body: azureToken
     };
 }
